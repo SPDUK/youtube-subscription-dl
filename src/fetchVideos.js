474a7f1d40d71d3834fcf4629af56fe1c9e512f5
spduk <spdevuk@gmail.com>
2019-04-20 17:10:23 +0100

    feat: add retrying and history of retries

--- src/fetchVideos.js
+++ src/fetchVideos.js
@@ -4,12 +4,12 @@ const fs = require('fs');
 
 const historyPath = './history.json';
 // updates previous history if passed in, otherwise it's empty
-function updateHistory(previous = []) {
+function updateHistory(retry = {}) {
   fs.writeFileSync(
     historyPath,
     JSON.stringify({
       last: Date.now(),
-      previous
+      retry
     })
   );
 }
@@ -19,15 +19,36 @@ if (!fs.existsSync(historyPath)) {
 }
 const history = JSON.parse(fs.readFileSync(historyPath, 'utf8'));
 
+// download a single video, if we can't download it then add it to the retry table
+function download(id) {
+  const dl = shell.exec(`youtube-dl youtu.be/${id}`);
+  if (dl.code) {
+    history.retry[id].count = history.retry[id].count + 1 || 1;
+  }
+}
+
+// checks if a video is new, if it is we download it
 function getNewVideos(video) {
   const videoDate = new Date(video.snippet.publishedAt).getTime();
-  const vidId = video.snippet.resourceId.videoId;
+  const id = video.snippet.resourceId.videoId;
 
   // if the item is newer than the last time we fetched videos and it has not been seen before
-  if (videoDate > history.last && !history.previous[vidId]) {
-    history.previous[vidId] = true;
-    shell.exec(`youtube-dl youtu.be/${vidId}`);
-  }
+  if (videoDate > history.last) download(id);
+}
+
+// object of retries that need to be done
+function retryDownload(retries) {
+  const ids = Object.keys(retries);
+  ids.forEach(id => {
+    // if we retried 4 times before, remove the id from the retry table
+    if (history.retry[id].count > 4) {
+      delete history.retry[id];
+    } else {
+      download(id);
+    }
+  });
+  // update history with new retries if we had to update them
+  updateHistory(history.retry);
 }
 
 async function fetchVideos(auth) {
@@ -36,7 +57,7 @@ async function fetchVideos(auth) {
   const subscriptions = await service.subscriptions.list({
     mine: true,
     auth,
-    part: 'snippet,contentDetails'
+    part: 'snippet'
   });
   // array of each channel you're subscribed to
   const channels = subscriptions.data.items;
@@ -47,14 +68,13 @@ async function fetchVideos(auth) {
     const channelInfo = await service.channels.list({
       id,
       auth,
-      part: 'snippet,contentDetails'
+      part: 'contentDetails'
     });
     // destructure the uploads playlist (all channel videos) and the channel title
     const {
       contentDetails: {
         relatedPlaylists: { uploads }
-      },
-      snippet: { title }
+      }
     } = channelInfo.data.items[0];
 
     const {
@@ -62,12 +82,16 @@ async function fetchVideos(auth) {
     } = await service.playlistItems.list({
       playlistId: uploads,
       auth,
-      part: 'snippet,contentDetails',
+      part: 'snippet',
       maxResults: 20 // 20? not many people upload more than 20 videos per day?
     });
     items.forEach(getNewVideos);
-
-    updateHistory(history.previous);
+  }
+  // if there is current videos that have failed download - retry them this time
+  if (Object.keys(history.retry).length) {
+    retryDownload(history.retry);
+  } else {
+    updateHistory();
   }
 }
 
