0c6d9b2ebd7d2e6682a118ab4d914b080fa29972
spduk <spdevuk@gmail.com>
2019-04-20 13:37:18 +0100

    feat: clean up code and get basic subscription download working

---
+++ src/fetchVideos.js
@@ -0,0 +1,44 @@
+const { google } = require('googleapis');
+const shell = require('shelljs');
+
+async function fetchVideos(auth) {
+  const service = google.youtube('v3');
+  // get a list of all current subscriptions
+  const subscriptions = await service.subscriptions.list({
+    mine: true,
+    auth,
+    part: 'snippet,contentDetails'
+  });
+  // array of each channel you're subscribed to
+  const channels = subscriptions.data.items;
+
+  for (const channel of channels) {
+    const id = channel.snippet.resourceId.channelId;
+    // get list of all channel uploads
+    const channelInfo = await service.channels.list({
+      id,
+      auth,
+      part: 'snippet,contentDetails'
+    });
+    // destructure the uploads playlist (all channel videos) and the channel title
+    const {
+      contentDetails: {
+        relatedPlaylists: { uploads }
+      },
+      snippet: { title }
+    } = channelInfo.data.items[0];
+    console.log(title);
+    console.log(uploads);
+
+    const ups = await service.playlistItems.list({
+      playlistId: uploads,
+      auth,
+      part: 'snippet,contentDetails'
+    });
+    const vidId = ups.data.items[0].snippet.resourceId.videoId;
+    // console.log(vidId);
+    // shell.exec(`youtube-dl youtu.be/${vidId}`); this works
+  }
+}
+
+module.exports = fetchVideos;
--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
-var fs = require('fs');
-var readline = require('readline');
-var { google } = require('googleapis');
-var OAuth2 = google.auth.OAuth2;
+const fs = require('fs');
+const shell = require('shelljs');
+
+const readline = require('readline');
+const { google } = require('googleapis');
+
+const { OAuth2 } = google.auth;
+const fetchVideos = require('./fetchVideos');
+
+const ver = shell.exec('youtube-dl --version');
+// if there's an error code (not 0) throw an error because youtube-dl isn't installed
+if (ver.code) {
+  throw new Error(
+    'youtube-dl must be installed! https://ytdl-org.github.io/youtube-dl/download.html'
+  );
+}
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/youtube-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/userinfo.profile'];
-var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH || process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'youtube-nodejs-quickstart.json';
+const SCOPES = ['https://www.googleapis.com/auth/userinfo.profile'];
+const TOKEN_DIR = `${process.env.HOME ||
+  process.env.HOMEPATH ||
+  process.env.USERPROFILE}/.credentials/`;
+const TOKEN_PATH = `${TOKEN_DIR}youtube-nodejs-quickstart.json`;
 
 // Load client secrets from a local file.
 fs.readFile('client_secret.json', function processClientSecrets(err, content) {
   if (err) {
-    console.log('Error loading client secret file: ' + err);
+    console.log(`Error loading client secret file: ${err}`);
     return;
   }
   // Authorize a client with the loaded credentials, then call the YouTube API.
-  authorize(JSON.parse(content), getChannel);
+  authorize(JSON.parse(content), fetchVideos);
 });
 
 /**
@@ -27,10 +41,10 @@ fs.readFile('client_secret.json', function processClientSecrets(err, content) {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
+  const clientSecret = credentials.installed.client_secret;
+  const clientId = credentials.installed.client_id;
+  const redirectUrl = credentials.installed.redirect_uris[0];
+  const oauth2Client = new OAuth2(clientId, clientSecret, redirectUrl);
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, function(err, token) {
@@ -52,12 +66,12 @@ function authorize(credentials, callback) {
  *     client.
  */
 function getNewToken(oauth2Client, callback) {
-  var authUrl = oauth2Client.generateAuthUrl({
+  const authUrl = oauth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES
   });
   console.log('Authorize this app by visiting this url: ', authUrl);
-  var rl = readline.createInterface({
+  const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
@@ -84,56 +98,13 @@ function storeToken(token) {
   try {
     fs.mkdirSync(TOKEN_DIR);
   } catch (err) {
-    if (err.code != 'EEXIST') {
+    if (err.code !== 'EEXIST') {
       throw err;
     }
   }
   fs.writeFile(TOKEN_PATH, JSON.stringify(token), err => {
     if (err) throw err;
-    console.log('Token stored to ' + TOKEN_PATH);
+    console.log(`Token stored to ${TOKEN_PATH}`);
   });
-  console.log('Token stored to ' + TOKEN_PATH);
-}
-
-/**
- * Lists the names and IDs of up to 10 files.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-async function getChannel(auth) {
-  var service = google.youtube('v3');
-  service.subscriptions.list(
-    {
-      mine: true,
-      auth: auth,
-      part: 'snippet,contentDetails'
-    },
-    async function(err, response) {
-      if (err) {
-        console.log('The API returned an error: ' + err);
-        return;
-      }
-      var channels = response.data.items;
-      if (channels.length == 0) {
-        console.log('No channel found.');
-      } else {
-        const id = channels[0].snippet.resourceId.channelId;
-        console.log(id)
-        const x = await service.channels.list({
-          id,
-          auth: auth,
-          part: 'snippet,contentDetails,statistics'
-        });
-
-        const upId = x.data.items[0].contentDetails.relatedPlaylists.uploads;
-        console.log(upId);
-        const ups = await service.playlistItems.list({
-          playlistId: upId,
-          auth: auth,
-          part: 'snippet,contentDetails'
-        });
-        console.log(ups.data.items[0]);
-      }
-    }
-  );
+  console.log(`Token stored to ${TOKEN_PATH}`);
 }
