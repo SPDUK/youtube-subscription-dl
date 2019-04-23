9fd7e2150053beb0b463e060ff1e67616d54af07
spduk <spdevuk@gmail.com>
2019-04-23 10:52:01 +0100

    fix: make notifications correctly display from inside a cronjob

--- yt-subs.sh
+++ yt-subs.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+# https://github.com/mikaelbr/node-notifier/issues/226
+export XDG_RUNTIME_DIR=/run/user/$(id -u) # fixes notifications not displaying from inside cron 
+
 DIR="$(cd "$(dirname "$0")" && pwd)"
 node $DIR/src/index.js
